b9e658cac1f475fc8589a30ec27773f82957c2bf
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-12-10 20:21:29 +0100

    Timer: Busy wait

---
+++ Timer/Simple timer usage - Busy wait/Core/Inc/casovnik.h
@@ -0,0 +1,18 @@
+/*
+ * casovnik.h
+ *
+ *  Created on: 10.12.2020.
+ *      Author: Ika
+ */
+
+#ifndef CORE_INC_CASOVNIK_H_
+#define CORE_INC_CASOVNIK_H_
+
+#include "main.h"
+#include "tim.h"
+#include "gpio.h"
+
+extern void casovnik();
+
+
+#endif /* CORE_INC_CASOVNIK_H_ */
