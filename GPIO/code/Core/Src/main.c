c42a0af374074bf9278442381085123366f7e6f7
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-12-10 09:49:49 +0100

    GPIO: How to SET pin value to 1

--- GPIO/code/Core/Src/main.c
+++ GPIO/code/Core/Src/main.c
@@ -86,7 +86,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
