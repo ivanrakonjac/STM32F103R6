2b5cb91dc1b1a5c7dabf5b3b4b223827064efbf1
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-02 14:45:02 +0100

    Lab 1: MIPS ETF, STM32 initialization

---
+++ Lab 1/src/main.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+uint8_t const rodata[] = "IVAN RAKONJAC";
+uint8_t data[] = "2020 SI";
+
+uint32_t bss;
+
+uint32_t main ( ) {
+
+	bss = 0;
+
+	while (1) {
+	
+	}
+	
+	return 0;
+}
