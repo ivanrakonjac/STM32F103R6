7707d7cb0302131cfe1e3c7974202b25d4d26e81
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-14 23:01:15 +0100

    FreeRTOS: Queues basics ( catch EXTI interrupt, put GPIO_Pin index in the queue, create a task that reads GPIO_Pin indexes from the queue, and toggles values on thees pins).

--- FreeRTOS/Core/Src/example.c
+++ FreeRTOS/Core/Src/example.c
@@ -1,25 +1,32 @@
 #include "FreeRTOS.h"
 #include "timers.h"
+#include "queue.h"
 #include "example.h"
 #include "gpio.h"
 #include "task.h"
 
+QueueHandle_t queueHandle;
 
-void callbackDeffered (void* p1, uint32_t p2);
+
+void taskToggler (void* parameters);
 
 
 void exampleInit() {
-
+	queueHandle = xQueueCreate(10, sizeof(uint32_t));
+	xTaskCreate(taskToggler, "taskToggler", 128, NULL, 2, NULL);
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-	if (GPIO_Pin == GPIO_PIN_0) {
-		BaseType_t  woken = pdFALSE;
-		xTimerPendFunctionCallFromISR(callbackDeffered, NULL, 0, &woken);
-		portYIELD_FROM_ISR(woken);
-	}
+	uint32_t itemToQueue = GPIO_Pin;
+	BaseType_t  woken = pdFALSE;
+	xQueueSendToBackFromISR(queueHandle, &itemToQueue, &woken);
+	portYIELD_FROM_ISR(woken);
 }
 
-void callbackDeffered (void* p1, uint32_t p2) {
-	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+void taskToggler (void* parameters) {
+	uint32_t bufferForPinIndex;
+	while (1) {
+		xQueueReceive(queueHandle, &bufferForPinIndex, portMAX_DELAY);
+		HAL_GPIO_TogglePin(GPIOC, bufferForPinIndex);
+	}
 }
