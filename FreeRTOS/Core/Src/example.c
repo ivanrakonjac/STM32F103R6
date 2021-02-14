d4740bd2acb77e2be88350cd219113de7bc598ae
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-14 20:09:26 +0100

    FreeRTOS: Create 2 tasks with equal priority using the same function

--- FreeRTOS/Core/Src/example.c
+++ FreeRTOS/Core/Src/example.c
@@ -6,14 +6,16 @@
 void exampleTaskFunction (void* parameters);
 
 void exampleInit() {
-	xTaskCreate(exampleTaskFunction, "exampleTaskFunction", 128, NULL, 1, NULL);
+	xTaskCreate(exampleTaskFunction, "exampleTaskFunction0", 128, (void*) GPIO_PIN_0, 1, NULL);
+	xTaskCreate(exampleTaskFunction, "exampleTaskFunction1", 128, (void*) GPIO_PIN_1, 1, NULL);
+
 }
 
 
 void exampleTaskFunction (void* parameters) {
 
 	while(1) {
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+		HAL_GPIO_TogglePin(GPIOC, (uint32_t) parameters);
 		vTaskDelay(1000);
 	}
 }
