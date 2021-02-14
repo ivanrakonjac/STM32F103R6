d3bb5e36970b845d5fff80ba5c1a52943f193a01
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-14 20:46:56 +0100

    FreeRTOS: Notification sending between 2 tasks is implemented.

--- FreeRTOS/Core/Src/example.c
+++ FreeRTOS/Core/Src/example.c
@@ -3,21 +3,28 @@
 #include "gpio.h"
 #include "task.h"
 
-void exampleTaskFunction (void* parameters);
+void taskController (void* parameters);
+void taskToggler (void* paramteres);
+
+TaskHandle_t controllerHandle;
+TaskHandle_t togglerHandle;
 
 void exampleInit() {
-	xTaskCreate(exampleTaskFunction, "exampleTaskFunction0", 128, (void*) GPIO_PIN_0, 2, NULL);
-	xTaskCreate(exampleTaskFunction, "exampleTaskFunction1", 128, (void*) GPIO_PIN_1, 1, NULL);
+	xTaskCreate(taskController, "controller", 128, NULL, 2, &controllerHandle);
+	xTaskCreate(taskToggler, "toggler", 128, NULL, 1, &togglerHandle);
 
 }
 
+void taskController (void* parameters) {
+	while (1) {
+		xTaskNotifyGive(togglerHandle);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
 
-void exampleTaskFunction (void* parameters) {
-
-	while(1) {
-		HAL_GPIO_TogglePin(GPIOC, (uint32_t) parameters);
-		for (int i = 0; i < 1000000; ++i) {
-
-		}
+void taskToggler (void* paramteres) {
+	while (1) {
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
 	}
 }
