173511c49c8459d6acf1f832b4cc05e33c735d9a
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-14 23:46:02 +0100

    FreeRTOS: How to create and use Software Timers

--- FreeRTOS/Core/Src/example.c
+++ FreeRTOS/Core/Src/example.c
@@ -7,20 +7,40 @@
 
 QueueHandle_t queueHandle;
 
+TimerHandle_t timer1;
+TimerHandle_t timer2;
+
 
 void taskToggler (void* parameters);
+void callbackTimer(TimerHandle_t timerHandle);
 
 
 void exampleInit() {
 	queueHandle = xQueueCreate(10, sizeof(uint32_t));
 	xTaskCreate(taskToggler, "taskToggler", 128, NULL, 2, NULL);
+
+	timer1 = xTimerCreate("seconds1", pdMS_TO_TICKS(1000), pdTRUE, (void*) GPIO_PIN_0, callbackTimer);
+	timer2 = xTimerCreate("seconds3", pdMS_TO_TICKS(3000), pdTRUE, (void*) GPIO_PIN_1, callbackTimer);
+
+	xTimerStart(timer1, portMAX_DELAY);
+	xTimerStart(timer2, portMAX_DELAY);
+
+}
+
+void callbackTimer(TimerHandle_t timerHandle){
+	uint32_t GPIO_Pin = (uint32_t) pvTimerGetTimerID(timerHandle);
+	HAL_GPIO_TogglePin(GPIOC, GPIO_Pin);
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-	uint32_t itemToQueue = GPIO_Pin;
-	BaseType_t  woken = pdFALSE;
-	xQueueSendToBackFromISR(queueHandle, &itemToQueue, &woken);
-	portYIELD_FROM_ISR(woken);
+
+	if(GPIO_Pin == GPIO_PIN_2 || GPIO_PIN_3){
+		uint32_t itemToQueue = GPIO_Pin;
+		BaseType_t  woken = pdFALSE;
+		xQueueSendToBackFromISR(queueHandle, &itemToQueue, &woken);
+		portYIELD_FROM_ISR(woken);
+	}
+
 }
 
 void taskToggler (void* parameters) {
