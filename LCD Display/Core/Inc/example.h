d5efb5f6dad746fa104ebf069a9401140fa720ce
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-15 20:44:14 +0100

    How to create a Task that writes something on LCD

---
+++ LCD Display/Core/Inc/example.h
@@ -0,0 +1,15 @@
+/*
+ * example.h
+ *
+ *  Created on: 14.02.2021.
+ *      Author: Ika
+ */
+
+#ifndef CORE_INC_EXAMPLE_H_
+#define CORE_INC_EXAMPLE_H_
+
+
+extern void exampleInit();
+
+
+#endif /* CORE_INC_EXAMPLE_H_ */
--- LCD Display/Core/Src/freertos.c
+++ LCD Display/Core/Src/freertos.c
@@ -26,7 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "example.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
