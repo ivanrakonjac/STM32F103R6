69c8068df0b683f5e049f88a4f344143668e6cf1
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-15 21:26:38 +0100

    How to create and display a character on LCD

--- LCD Display/Core/Src/example.c
+++ LCD Display/Core/Src/example.c
@@ -34,18 +34,37 @@ static void exampleTask (void* parameters) {
 	}
 
 	// Shift display
-	for (uint32_t i = 0; i < 10; i++){
+	/*for (uint32_t i = 0; i < 10; i++){
 		LCD_CommandEnqueue(LCD_INSTRUCTION,
 		LCD_SHIFT_INSTRUCTION |
 		LCD_SHIFT_DISPLAY |
 		LCD_SHIFT_DIRECTION_RIGHT);
 		vTaskDelay(pdMS_TO_TICKS(100));
-	}
+	}*/
+
+	// Write to CGRAM
+	LCD_CommandEnqueue(LCD_INSTRUCTION,
+	LCD_SET_CG_RAM_ADDRESS_INSTRUCTION | 0x08);
+
+	LCD_CommandEnqueue(LCD_DATA, 0x04); //		_ _ X _ _
+	LCD_CommandEnqueue(LCD_DATA, 0x02); //		_ _ _ X _
+	LCD_CommandEnqueue(LCD_DATA, 0x11); //		X _ _ _ X
+	LCD_CommandEnqueue(LCD_DATA, 0x01); //		_ _ _ _ X
+	LCD_CommandEnqueue(LCD_DATA, 0x11); //		X _ _ _ X
+	LCD_CommandEnqueue(LCD_DATA, 0x02); //		_ _ _ X _
+	LCD_CommandEnqueue(LCD_DATA, 0x04); //		_ _ X _ _
+
+	// Prikazujemo ga u drugom redu nakon sistemi
+	LCD_CommandEnqueue(LCD_INSTRUCTION,
+	LCD_SET_DD_RAM_ADDRESS_INSTRUCTION | 0x48);
+
+	// Stavljamo adresu u red sa koje treba iscitati karakter
+	LCD_CommandEnqueue(LCD_DATA, 0x01);
 
 
 
 	while(1){
-
+		vTaskDelay(pdMS_TO_TICKS(1000));
 	}
 }
 
